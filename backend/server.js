448a3edd3afe36c5d06d581eba80bc9d8e2b62fa
Joyal Shine <101260206+joyalshine@users.noreply.github.com>
2024-09-10 23:02:37 +0530

    Port changed

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const { initializeWebSocketServer } = require('./services/websocketService');
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use('/', historicalRoutes);
 
